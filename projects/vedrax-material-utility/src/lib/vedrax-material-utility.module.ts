ea0f4dbb8da41fcac68a1c097e4bb5a2d11db731
Remy Penchenat <finance@vedrax.com>
2022-09-12 14:25:58 +0300

    update app module

--- projects/vedrax-material-utility/src/lib/vedrax-material-utility.module.ts
+++ projects/vedrax-material-utility/src/lib/vedrax-material-utility.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DecimalPipe } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+//angular library
+import { VedraxMaterialModule } from './material/vedrax-material.module';
 
 
 @NgModule({
   declarations: [
   ],
   imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    VedraxMaterialModule,
+    RouterModule
   ],
   exports: [
+    VedraxMaterialModule
   ]
 })
 export class VedraxMaterialUtilityModule { }
