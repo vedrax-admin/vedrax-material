0b4712c3fd59ec16feb425666be45a3d1b76fd08
Remy Penchenat <finance@vedrax.com>
2022-09-12 17:17:21 +0300

    ADD INPUT COMPONENT

---
+++ projects/vedrax-material-utility/src/lib/components/vedrax-input/vedrax-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { VedraxBaseComponent } from '../vedrax-base.component';
+
+/**
+ * Class that defines an input component
+ */
+@Component({
+  selector: 'vedrax-input',
+  templateUrl: './vedrax-input.component.html'
+})
+export class VedraxInputComponent extends VedraxBaseComponent {}
