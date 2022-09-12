b12953a934ff95bb8c136af3ff66d35a3df8834d
Remy Penchenat <finance@vedrax.com>
2022-09-12 14:22:54 +0300

    add material module

--- projects/vedrax-material-utility/src/lib/vedrax-material-utility.module.ts
+++ projects/vedrax-material-utility/src/lib/vedrax-material-utility.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { VedraxMaterialUtilityComponent } from './vedrax-material-utility.component';
-
 
 
 @NgModule({
   declarations: [
-    VedraxMaterialUtilityComponent
   ],
   imports: [
   ],
   exports: [
-    VedraxMaterialUtilityComponent
   ]
 })
 export class VedraxMaterialUtilityModule { }
