4078391743288cde303f81da994b9cbc3900e923
Remy Penchenat <finance@vedrax.com>
2022-09-20 21:09:47 +0300

    add app module in interface

--- projects/vedrax-material-utility/src/public-api.ts
+++ projects/vedrax-material-utility/src/public-api.ts
@@ -6,6 +6,8 @@ export * from './lib/components';
 export * from './lib/config';
 export * from './lib/entities';
 export * from './lib/enum';
-export * from './lib/services';
 export * from './lib/material/vedrax-material.module';
+export * from './lib/services';
+export * from './lib/vedrax-material-utility.module';
+
 
